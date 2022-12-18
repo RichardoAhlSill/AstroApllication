e7cc63c5f7251523d165d109068a4e9436cb4345
Icami747 <103399414+Icami747@users.noreply.github.com>
2022-12-18 10:56:38 -0300

    Mudanças por Içami

---
+++ lib/data/api/questoes_api.dart
@@ -0,0 +1,9 @@
+import 'dart:convert';
+
+import 'package:astroapp/domain/questoes.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+
+class QuestoesApi {
+  String baseUrl = "https://api-production-b057.up.railway.app/";
+}
---
+++ lib/data/api/user_api.dart
@@ -0,0 +1,9 @@
+import 'dart:convert';
+
+import 'package:astroapp/domain/user.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+
+class UsuariosApi {
+  String baseUrl = "https://api-production-b057.up.railway.app/";
+}
